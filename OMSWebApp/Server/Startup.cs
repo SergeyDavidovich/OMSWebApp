e7fb85d39cc5197d9878babd6f7e443b288b21e8
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-09-04 12:13:15 +0200

    added Select Order in Order journal

--- OMSWebApp/Server/Startup.cs
+++ OMSWebApp/Server/Startup.cs
@@ -30,6 +30,9 @@ namespace OMSWebApp.Server
             services.AddDbContext<NorthwindContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection"))
                 );
+            // https://stackoverflow.com/questions/59199593/net-core-3-0-possible-object-cycle-was-detected-which-is-not-supported
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
