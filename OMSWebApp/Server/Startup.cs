9e2df817eed0ab6bf57237681c2c3ad24424353f
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-09-06 01:53:10 +0200

    no comments

--- OMSWebApp/Server/Startup.cs
+++ OMSWebApp/Server/Startup.cs
@@ -49,7 +49,7 @@ namespace OMSWebApp.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            //app.UseCors();
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
